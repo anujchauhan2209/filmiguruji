45726c509fc305e82494a331f52cbb0681e07aed
Anuj Singh Chauhan <151845943+anujchauhan2209@users.noreply.github.com>
2025-06-14 19:39:53 +0530

    done
    ok

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Initialize Video Carousel
+$(document).ready(function(){
+    $('.video-carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    });
+});
